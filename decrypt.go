3d22032f6d6b9f4745af41cd0d47668699fabe5d
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-05-12 15:45:43 +1000

    move functions into new files

---
+++ decrypt.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"os"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
+	"github.com/aixoio/aesbuddy"
+)
+
+func decryptWindow(w fyne.Window) fyne.CanvasObject {
+	backbtn := widget.NewButtonWithIcon("Back to menu", theme.NavigateBackIcon(), func() { w.SetContent(render(w)) })
+	filePathTxt := widget.NewLabel("No file selected")
+	path := ""
+	pwdWid := widget.NewPasswordEntry()
+
+	pwdWid.SetPlaceHolder("Password")
+
+	decBtn := widget.NewButton("Decrypt", func() {
+		dat, err := os.ReadFile(path)
+		if err != nil {
+			dialog.ShowError(err, w)
+			w.Close()
+		}
+
+		aes_key := sha256_to_bytes([]byte(pwdWid.Text))
+
+		deced, err := aesbuddy.AesCBCDecrypt(aes_key, dat)
+		if err != nil {
+			dialog.ShowError(err, w)
+			w.Close()
+		}
+
+		dialog.ShowFileSave(func(uc fyne.URIWriteCloser, err error) {
+			if err != nil {
+				w.Close()
+			}
+
+			_, err = uc.Write(deced)
+			if err != nil {
+				dialog.ShowError(err, w)
+				w.Close()
+			}
+
+			w.Close()
+
+		}, w)
+
+	})
+
+	decBtn.Disable()
+
+	pwdWid.OnChanged = func(s string) {
+		if len(s) != 0 && path != "" {
+			decBtn.Enable()
+		} else {
+			decBtn.Disable()
+		}
+	}
+
+	return container.NewBorder(
+		container.NewGridWithColumns(
+			3,
+			backbtn,
+			widget.NewLabel("Decrypt"),
+			widget.NewLabel(""),
+		),
+		decBtn,
+		nil,
+		nil,
+		container.New(
+			layout.NewFormLayout(),
+			widget.NewLabel("File"),
+			container.NewGridWithColumns(
+				2,
+				filePathTxt,
+				widget.NewButton("Select file", func() {
+					dialog.ShowFileOpen(func(uc fyne.URIReadCloser, err error) {
+						if err != nil {
+							w.Close()
+						}
+						path = uc.URI().Path()
+						filePathTxt.SetText(uc.URI().Name())
+						if len(pwdWid.Text) != 0 && path != "" {
+							decBtn.Enable()
+						} else {
+							decBtn.Disable()
+						}
+					}, w)
+				}),
+			),
+			widget.NewLabel("Password"),
+			pwdWid,
+		),
+	)
+}
---
+++ encrypt.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"os"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
+	"github.com/aixoio/aesbuddy"
+)
+
+func encryptWindow(w fyne.Window) fyne.CanvasObject {
+	backbtn := widget.NewButtonWithIcon("Back to menu", theme.NavigateBackIcon(), func() { w.SetContent(render(w)) })
+	filePathTxt := widget.NewLabel("No file selected")
+	path := ""
+	pwdWid := widget.NewPasswordEntry()
+
+	pwdWid.SetPlaceHolder("Password")
+
+	encBtn := widget.NewButton("Encrypt", func() {
+		dat, err := os.ReadFile(path)
+		if err != nil {
+			dialog.ShowError(err, w)
+			w.Close()
+		}
+
+		aes_key := sha256_to_bytes([]byte(pwdWid.Text))
+
+		enced, err := aesbuddy.AesCBCEncrypt(aes_key, dat)
+		if err != nil {
+			dialog.ShowError(err, w)
+			w.Close()
+		}
+
+		dialog.ShowFileSave(func(uc fyne.URIWriteCloser, err error) {
+			if err != nil {
+				w.Close()
+			}
+
+			_, err = uc.Write(enced)
+			if err != nil {
+				dialog.ShowError(err, w)
+				w.Close()
+			}
+
+			w.Close()
+
+		}, w)
+
+	})
+
+	encBtn.Disable()
+
+	pwdWid.OnChanged = func(s string) {
+		if len(s) != 0 && path != "" {
+			encBtn.Enable()
+		} else {
+			encBtn.Disable()
+		}
+	}
+
+	return container.NewBorder(
+		container.NewGridWithColumns(
+			3,
+			backbtn,
+			widget.NewLabel("Encrypt"),
+			widget.NewLabel(""),
+		),
+		encBtn,
+		nil,
+		nil,
+		container.New(
+			layout.NewFormLayout(),
+			widget.NewLabel("File"),
+			container.NewGridWithColumns(
+				2,
+				filePathTxt,
+				widget.NewButton("Select file", func() {
+					dialog.ShowFileOpen(func(uc fyne.URIReadCloser, err error) {
+						if err != nil {
+							w.Close()
+						}
+						path = uc.URI().Path()
+						filePathTxt.SetText(uc.URI().Name())
+						if len(pwdWid.Text) != 0 && path != "" {
+							encBtn.Enable()
+						} else {
+							encBtn.Disable()
+						}
+					}, w)
+				}),
+			),
+			widget.NewLabel("Password"),
+			pwdWid,
+		),
+	)
+
+}
