393283535b4945a0181ea3a3f212bddc8084f0e9
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-05-12 15:08:09 +1000

    Add decrypt function

--- render.go
+++ render.go
@@ -28,7 +28,7 @@ func render() fyne.CanvasObject {
 				}),
 				widget.NewButton("Decrypt", func() {
 					w := a.NewWindow("Decrypt")
-					w.SetContent(decryptWindow())
+					w.SetContent(decryptWindow(w))
 					w.Resize(fyne.NewSize(512, 512))
 					w.Show()
 				}),
@@ -119,6 +119,83 @@ func encryptWindow(w fyne.Window) fyne.CanvasObject {
 
 }
 
-func decryptWindow() fyne.CanvasObject {
-	return widget.NewLabel("Decrypt")
+func decryptWindow(w fyne.Window) fyne.CanvasObject {
+	filePathTxt := widget.NewLabel("No file selected")
+	path := ""
+	pwdWid := widget.NewPasswordEntry()
+
+	pwdWid.SetPlaceHolder("Password")
+
+	decBtn := widget.NewButton("Decrypt", func() {
+		dat, err := os.ReadFile(path)
+		if err != nil {
+			dialog.ShowError(err, w)
+			w.Close()
+		}
+
+		aes_key := sha256_to_bytes([]byte(pwdWid.Text))
+
+		deced, err := aesbuddy.AesCBCDecrypt(aes_key, dat)
+		if err != nil {
+			dialog.ShowError(err, w)
+			w.Close()
+		}
+
+		dialog.ShowFileSave(func(uc fyne.URIWriteCloser, err error) {
+			if err != nil {
+				w.Close()
+			}
+
+			_, err = uc.Write(deced)
+			if err != nil {
+				dialog.ShowError(err, w)
+				w.Close()
+			}
+
+			w.Close()
+
+		}, w)
+
+	})
+
+	decBtn.Disable()
+
+	pwdWid.OnChanged = func(s string) {
+		if len(s) != 0 && path != "" {
+			decBtn.Enable()
+		} else {
+			decBtn.Disable()
+		}
+	}
+
+	return container.NewBorder(
+		widget.NewLabel("Decrypt"),
+		decBtn,
+		nil,
+		nil,
+		container.New(
+			layout.NewFormLayout(),
+			widget.NewLabel("File"),
+			container.NewGridWithColumns(
+				2,
+				filePathTxt,
+				widget.NewButton("Select file", func() {
+					dialog.ShowFileOpen(func(uc fyne.URIReadCloser, err error) {
+						if err != nil {
+							w.Close()
+						}
+						path = uc.URI().Path()
+						filePathTxt.SetText(uc.URI().Name())
+						if len(pwdWid.Text) != 0 && path != "" {
+							decBtn.Enable()
+						} else {
+							decBtn.Disable()
+						}
+					}, w)
+				}),
+			),
+			widget.NewLabel("Password"),
+			pwdWid,
+		),
+	)
 }
